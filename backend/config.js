fe03e794b33f31d5df4c57b36edaaec8cb0f6bbe
Akshay4504 <akshayanand4504@gmail.com>
2025-11-13 20:40:17 +0530

    Initial commit

---
+++ backend/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	USER: 'akshayanand4504@gmail.com',
+	PASS: 'jpycdpzecttjxuux',
+	EMAIL: 'akshayanand4504@gmail.com',
+	HOST: 'smtp.gmail.com',
+	MAILPORT: 465, 
+	YOURNAME: 'Akshay Anand',
+	YOURSITE: 'https://www.akshayanand.in', 
+};
